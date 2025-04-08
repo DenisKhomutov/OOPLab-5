370f1749b4bf37986f60632da9883fd833c24b8e
DenisKhomutov <carroltonnorcez@gmail.com>
2025-04-08 21:58:03 +0500

    Add files via upload

---
+++ OOP№5part2/OOP№5part2/OOP№5part2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+void func1(Base obj) {
+
+};
+void func2(Base* obj) {
+
+};
+void func3(Base& obj) {
+
+};
+class Base {
+
+};
+class Desc : public Base {
+
+};
+int main() {
+
+}
