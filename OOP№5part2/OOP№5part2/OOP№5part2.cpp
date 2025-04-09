e2279d8932ef5c0166337de3e27a0f524034f63a
DenisKhomutov <carroltonnorcez@gmail.com>
2025-04-09 10:21:44 +0500

    Update OOP№5part2.cpp Передача объектов Base и Desc в функции

--- OOP№5part2/OOP№5part2/OOP№5part2.cpp
+++ OOP№5part2/OOP№5part2/OOP№5part2.cpp
@@ -1,22 +1,77 @@
-﻿#include <iostream>
+#include <iostream>
 #include <stdio.h>
 using namespace std;
+
+class Base {
+public:
+	Base() {
+		printf("\nBase()");
+	}
+	Base(Base* obj) {
+		printf("\nBase(Base* obj)");
+	}
+	Base(Base& obj) {
+		printf("\nBase(Base& obj)");
+	}
+	virtual ~Base() {
+		printf("\n~Base()");
+	}
+};
+class Desc : public Base {
+public:
+	Desc() {
+		printf("\nDesc()");
+	}
+	Desc(Desc* obj) {
+		printf("\nDesc(Desc* obj)");
+	}
+	Desc(Desc& obj) {
+		printf("\nDesc(Desc& obj)");
+	}
+	~Desc() {
+		printf("\n~Desc()");
+	}
+};
 
 void func1(Base obj) {
 
 };
 void func2(Base* obj) {
-
+	Desc* newdesc = dynamic_cast<Desc*>(obj);
+	if (newdesc) {
+		printf("\nОбъект класса Desc");
+	}
+	else {
+		printf("\nОбъект другого класса");
+	}
 };
 void func3(Base& obj) {
 
 };
-class Base {
 
-};
-class Desc : public Base {
-
-};
 int main() {
+	setlocale(LC_ALL, "Russian");
+	printf("\n    ----|Передача объектов|----");
+	printf("\n   Конструктор Base: ");
+	Base base;
+	printf("\n   Функция func1: ");
+	func1(base);
+	printf("\n----");
+	printf("\n   Функция func2: ");
+	func2(&base);
+	printf("\n----");
+	printf("\n   Функция func3: ");
+	func3(base);
 
+	printf("\n\n   Конструктор Desc: ");
+	Desc desc;
+	printf("\n   Функция func1: ");
+	func1(desc);
+	printf("\n----");
+	printf("\n   Функция func2: ");
+	func2(&desc);
+	printf("\n----");
+	printf("\n   Функция func3: ");
+	func3(desc);
+	printf("\n\n   Деструкторы: ");
 }
